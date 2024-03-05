27266e80bc4d4880878cde4749cfd897269d3b72
u65042380102 <u65042380102@live.uru.ac.th>
2024-03-05 19:05:52 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [names, setNames] = useState([]);
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleAdd = () => {
+    if (name.trim() !== '') {
+      setNames([...names, name]);
+      setName('');
+    }
+  };
+
+  const handleDelete = (index) => {
+    const newNames = [...names];
+    newNames.splice(index, 1);
+    setNames(newNames);
+  };
+
+  const handleEdit = (index, newName) => {
+    const newNames = [...names];
+    newNames[index] = newName;
+    setNames(newNames);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>โปรแกรมเพิ่มลบแก้ไขข้อมูล</h1>
+      <input type="text" value={name} onChange={handleChange} />
+      <button className='add' onClick={handleAdd}>Add</button>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>
+            {name}
+            <span>
+            <button className = 'edit' onClick={() => handleEdit(index, prompt('กรอกชื่อที่ต้องการแก้ไข'))}>Edit</button>
+            <button className = 'delete' onClick={() => handleDelete(index)}>Delete</button>
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
